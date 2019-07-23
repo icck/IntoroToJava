93d6f49df222b11ba3b78f232288792c7c11b754
i-komori <ikom1111ikom@gmail.com>
2019-07-24 00:33:10 +0900

    array

---
+++ src/c4/array.java
@@ -0,0 +1,25 @@
+package c4;
+
+
+public class array {
+
+	public static void main(String[] args) {
+		int[] score = new int[5];
+
+		System.out.println(score.length);
+
+		score[0] = 4;
+
+		System.out.println(score[0]);
+
+		int[] score1 = new int[] {20, 30, 40, 50};
+		int[] score2 = {20, 30, 40, 50};
+
+		for (int i : score2) {
+			System.out.println(i);
+
+		}
+
+	}
+
+}
