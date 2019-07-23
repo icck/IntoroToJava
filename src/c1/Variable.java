7d54188dfb5876d3838b28d6545d1d7a76d0b3f3
i-komori <ikom1111ikom@gmail.com>
2019-07-23 22:26:10 +0900

    final practice

--- src/c1/Variable.java
+++ src/c1/Variable.java
@@ -3,8 +3,12 @@ package c1;
 public class Variable {
 
 	public static void main(String[] args) {
+		final int VAL = 5;
+		//VAL = 1; NG
 		int age = 10;
+
 		System.out.println("age:" + age);
+		System.out.println("VAL:" + VAL);
 
 	}
 
