419f0ab8d5ad1e853b285f305942ba60123da7ee
i-komori <ikom1111ikom@gmail.com>
2019-07-23 23:49:20 +0900

    if sample

---
+++ src/c3/ifSample.java
@@ -0,0 +1,19 @@
+package c3;
+
+public class ifSample {
+
+	public static void main(String[] args) {
+		boolean tenki = true;
+		if (tenki) {
+			System.out.println("ok");
+		} else {
+			System.out.println("ng");
+		}
+		String str = "str";
+		if (str.equals("str")) {
+			System.out.println("ok");
+		}
+
+	}
+
+}
