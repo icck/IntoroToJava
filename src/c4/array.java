8ebf33edf8a500b622b7f78949123be9916b7f59
i-komori <ikom1111ikom@gmail.com>
2019-07-24 00:38:59 +0900

    不要な変数をコメントアウト

--- src/c4/array.java
+++ src/c4/array.java
@@ -12,7 +12,7 @@ public class array {
 
 		System.out.println(score[0]);
 
-		int[] score1 = new int[] {20, 30, 40, 50};
+//		int[] score1 = new int[] {20, 30, 40, 50};
 		int[] score2 = {20, 30, 40, 50};
 
 		for (int i : score2) {
