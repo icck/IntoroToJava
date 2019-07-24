27f76a49b9a5ca800f1b30b78a7ee64dcc1dd7d8
icck <icck.n.n1111@gmail.com>
2019-07-24 23:02:02 +0900

    class

---
+++ src/c6/main/Calc.java
@@ -0,0 +1,13 @@
+package c6.main;
+
+import c6.logic.CalcLogic;
+
+public class Calc {
+
+	public static void main(String[] args) {
+		System.out.println(CalcLogic.tasu(1, 2));
+		System.out.println(CalcLogic.hiku(1, 2));
+
+	}
+
+}
