6939c36e014a2f285cb381fede0260f3f01add96
icck <icck.n.n1111@gmail.com>
2019-07-27 01:35:39 +0900

    class 作成とgetter setterまで

---
+++ src/c10/Hero.java
@@ -0,0 +1,23 @@
+package c10;
+
+public class Hero {
+    private int hp;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+}
---
+++ src/c10/Wand.java
@@ -0,0 +1,18 @@
+package c10;
+
+public class Wand {
+    private String Name;
+    private double power;
+    public String getName() {
+        return Name;
+    }
+    public void setName(String name) {
+        Name = name;
+    }
+    public double getPower() {
+        return power;
+    }
+    public void setPower(double power) {
+        this.power = power;
+    }
+}
