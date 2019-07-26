600a79145682b839836763e348ce999ca16ae69f
icck <icck.n.n1111@gmail.com>
2019-07-27 01:58:45 +0900

    add error handle

--- src/c10/Wand.java
+++ src/c10/Wand.java
@@ -3,30 +3,41 @@ package c10;
 public class Wand {
     private String Name;
     private double power;
+
     public String getName() {
         return Name;
+
     }
+
     public void setName(String name) {
 
         if (name == null ) {
             throw new IllegalArgumentException("nameにNullは設定できません");
+
         }
 
         if (name.length() < 3 ) {
             throw new IllegalArgumentException("nameは3文字以上を設定してください");
+
         }
 
         Name = name;
     }
+
     public double getPower() {
         return power;
+
     }
+
     public void setPower(double power) {
 
         if (power < 0.5 || power > 100) {
             throw new IllegalArgumentException("powerは0.5以上100以下を設定してください");
+
         }
 
         this.power = power;
+
     }
+
 }
--- src/c10/Wizard.java
+++ src/c10/Wizard.java
@@ -11,31 +11,77 @@ public class Wizard {
         int recovPoint = (int) (basePoint * this.getWand().getPower());
 
         h.setHp(h.getHp() + recovPoint);
+
         System.out.println(h.getName() + "のHPを" + recovPoint + "回復");
+
     }
 
     public int getHp() {
         return hp;
+
     }
+
     public void setHp(int hp) {
+
+        if (hp < 0) {
+            this.hp = 0;
+            return;
+
+        }
+
         this.hp = hp;
+
     }
+
     public int getMp() {
         return mp;
+
     }
+
     public void setMp(int mp) {
+
+        if (mp < 0) {
+            this.mp = 0;
+            return;
+
+        }
+
         this.mp = mp;
     }
+
     public String getName() {
         return name;
+
     }
+
     public void setName(String name) {
+
+        if (name == null ) {
+            throw new IllegalArgumentException("nameにNullは設定できません");
+
+        }
+
+        if (name.length() < 3 ) {
+            throw new IllegalArgumentException("nameは3文字以上を設定してください");
+
+        }
+
         this.name = name;
+
     }
+
     public Wand getWand() {
         return wand;
+
     }
+
     public void setWand(Wand wand) {
+
+        if (wand == null) {
+            throw new IllegalArgumentException("Wand is not null");
+
+        }
+
         this.wand = wand;
     }
 
