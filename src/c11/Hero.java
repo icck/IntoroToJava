22a68b115e734ae17d69a93bef9d25485cb1f574
icck <icck.n.n1111@gmail.com>
2019-07-27 02:24:16 +0900

    継承、オーバーライド

---
+++ src/c11/Hero.java
@@ -0,0 +1,15 @@
+package c11;
+
+public class Hero {
+
+    private final void sleep() {
+        System.out.println("sleep");
+
+    }
+
+    private void run() {
+        System.out.println("run");
+
+    }
+
+}
---
+++ src/c11/SupperHero.java
@@ -0,0 +1,15 @@
+package c11;
+
+public class SupperHero extends Hero{
+
+    private void fly() {
+        System.out.println("sleep");
+
+    }
+
+    private void run() {
+        System.out.println("run override");
+
+    }
+
+}
