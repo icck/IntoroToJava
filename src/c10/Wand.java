82a2479e28260b160936fd8eccd34e9d60132c05
icck <icck.n.n1111@gmail.com>
2019-07-27 01:46:09 +0900

    add error handle

--- src/c10/Wand.java
+++ src/c10/Wand.java
@@ -7,12 +7,26 @@ public class Wand {
         return Name;
     }
     public void setName(String name) {
+
+        if (name == null ) {
+            throw new IllegalArgumentException("nameにNullは設定できません");
+        }
+
+        if (name.length() < 3 ) {
+            throw new IllegalArgumentException("nameは3文字以上を設定してください");
+        }
+
         Name = name;
     }
     public double getPower() {
         return power;
     }
     public void setPower(double power) {
+
+        if (power < 0.5 || power > 100) {
+            throw new IllegalArgumentException("powerは0.5以上100以下を設定してください");
+        }
+
         this.power = power;
     }
 }
