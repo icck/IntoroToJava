87feab26f707f73c5ecf1ab4e56afb70b4d91e1d
icck <icck.n.n1111@gmail.com>
2019-07-27 08:55:48 +0900

    add constoractor

--- src/c11/Hero.java
+++ src/c11/Hero.java
@@ -2,6 +2,11 @@ package c11;
 
 public class Hero {
 
+    public Hero() {
+        System.out.println("Hero new");
+
+    }
+
     public final void sleep() {
         System.out.println("sleep");
 
--- src/c11/SupperHero.java
+++ src/c11/SupperHero.java
@@ -2,6 +2,12 @@ package c11;
 
 public class SupperHero extends Hero {
 
+    public SupperHero () {
+        super();
+        System.out.println("SupperHero new");
+
+    }
+
     public void fly() {
         System.out.println("sleep");
 
