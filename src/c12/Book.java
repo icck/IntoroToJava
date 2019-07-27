c47c600c0fdaec6c62acfbaaf443ad84ddd19152
icck <icck.n.n1111@gmail.com>
2019-07-27 14:11:35 +0900

    add

---
+++ src/c12/Book.java
@@ -0,0 +1,34 @@
+package c12;
+
+public class Book {
+    private String name;
+    private int price;
+    private String color;
+    private String isbn;
+
+    public Book (String name, int price, String color, String isbn) {
+        this.name = name;
+        this.price = price;
+        this.color = color;
+        this.isbn = isbn;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+}
